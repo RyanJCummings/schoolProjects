08eb68683e3dd88e20228586c565997f6a68c02a
Ryan Cummings <rcummings98@gmail.com>
2018-04-04 17:27:01 -0600

    First commit

---
+++ Maze.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg132lab5;
+
+/**
+ *
+ * @author ryancummings
+ */
+class Maze {
+    char[][] map = {
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+        {'#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#'},
+        {'.', '.', '#', '.', '#', '.', '#', '#', '#', '#', '.', '#'},
+        {'#', '#', '#', '.', '#', '.', '.', '.', '.', '#', '.', '#'},
+        {'#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '.', '#'},
+        {'#', '#', '#', '#', '.', '#', 'F', '#', '.', '#', '.', '#'},
+        {'#', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#'},
+        {'#', '#', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#'},
+        {'#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#'},
+        {'#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#'},
+        {'#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#'},
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+    };
+    
+    public Maze(){
+       
+    }
+    public void printMaze(){
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                System.out.print(map[i][j] + " ");   
+            }
+            System.out.println("");
+        }
+    }
+   /*
+        x refers to columns, y refers to rows
+    */
+    public int traverse(char[][] theMaze, int xLoc, int yLoc, int handLocationX, int handLocationY){
+        if (xLoc == 6 && yLoc == 5){
+            System.out.println("You are a winner!");
+            return 1;
+        }
+        else if (theMaze[handLocationY][handLocationX] == '#'){
+            
+            return traverse(theMaze, xLoc, yLoc, handLocationX, handLocationY);
+        }
+        else
+            return 0;
+    }
+    
+}
---
+++ Outlab5.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg132lab5;
+
+/**
+ *
+ * @author ryancummings
+ */
+public class Outlab5 {
+
+   
+
+    public static void main(String[] args) {
+        Maze myMaze = new Maze();
+        myMaze.printMaze();
+    }
+}
