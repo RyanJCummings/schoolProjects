cd2dc207f5b2d6eaa49af6991f1044c76753ad3a
SocratesFolly <rcummings98@gmail.com>
2018-11-17 11:57:06 -0700

    Menu created

--- Program.c
+++ Program.c
@@ -3,11 +3,44 @@
 
 typedef enum {false, true} bool;
 
+int printMenu();
+void classes_by_number();
+void classes_by_day();
+void classes_by_time();
+void classes_by_year();
+
 int main(int argv, char **argc){
-  int answer = 0;
-  while (answer != 5){
-    printf(//some stuff);
-  }
+  FILE *in_file = fopen("classes.txt", "r");
+  int answer;
+  do {
+      answer = printMenu();
+      if (answer == 1){
+        // print all classes by CSCI number
+      } else if (answer == 2){
+        // print classes by day/time group
+      } else if (answer == 3){
+        // print classes by time
+      } else if (answer == 4){
+        // print classes by availablity per year in school
+      } else{
+        // invalid response
+      }
+
+  } while(answer != 5);
 
   return 0;
+}
+
+// prints menu and stores user response
+int printMenu(){
+  int response;
+
+  printf("1) Print all classes by CSCI number (include all information) in order of CSCI number\n");
+  printf("2) Print all classes available on MWF or available on TR in order of times.\n");
+  printf("3) Print the class available at a specific time\n");
+  printf("4) Print classes available to freshmen, sophomores, juniors, or seniors\n");
+  printf("5) Quit\n");
+  scanf("%d", &response);
+
+  return response;
 }
