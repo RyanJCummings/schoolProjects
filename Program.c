f6b6f1134d45e58b3d68d955b97c82c54c0bfe29
SocratesFolly <rcummings98@gmail.com>
2018-11-17 10:15:26 -0700

    First commit

---
+++ Program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {false, true} bool;
+
+int main(int argv, char **argc){
+  int answer = 0;
+  while (answer != 5){
+    printf(//some stuff);
+  }
+
+  return 0;
+}
