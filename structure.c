fc4c274cfff087ad2b2ac3d1145f1d77e8ef5032
SocratesFolly <rcummings98@gmail.com>
2018-11-19 17:50:57 -0700

    syntax fix

--- structure.c
+++ structure.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 typedef struct csci_class {
-  char[20] course_num;
-  char[50] title;
-  char[5] day;
-  int[15] time;
-  int[3] year;
+  char course_num[20];
+  char title[20];
+  char day[5];
+  int time[15];
+  int year;
 } Class;
+
+int printMenu();
+void classes_by_number();
+void classes_by_day();
+void classes_by_time();
+void classes_by_year();
